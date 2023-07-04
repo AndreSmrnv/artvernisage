4fa38a1e93b80783d7881b32b8801d584f75481b
AndreSmrnv <tehmir@bk.ru>
2023-07-04 17:18:00 +0300

    add structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React    from 'react';
 import ReactDOM from 'react-dom/client';
+import {App}    from "./components/app/App";
 import                'normalize.css';
-import                './index.css';
+import                './main.modules.scss';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
