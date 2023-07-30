29a28bd38343209e29d3f027d6533af28c92065b
AndreSmrnv <tehmir@bk.ru>
2023-07-30 19:44:12 +0300

    check decrement count

--- src/components/count/Count.tsx
+++ src/components/count/Count.tsx
@@ -16,20 +16,25 @@ export const Count:FC<ICountProps> = (
     }
 ) => {
     const [count, setCount] = useState(value);
+    const [disableDecrement, setDisableDecrement] = useState(!!count);
 
     useEffect(
-        () => {onChange && onChange(count)},
+        () => {
+            onChange && onChange(count);
+            setDisableDecrement(count < 2)
+        },
         [count]
     );
 
     const handleIncrement = () => setCount( prev => prev + 1 );
-    const handleDecrement = () => !!count && setCount( prev => prev - 1 );
+    const handleDecrement = () => !disableDecrement && setCount( prev => prev - 1 );
 
     return (
         <div className={cn(s.count, className)}>
             <button type={'button'}
                     className={cn(s.item, s.btn)}
                     onClick={handleDecrement}
+                    disabled={disableDecrement}
             >
                 -
             </button>
